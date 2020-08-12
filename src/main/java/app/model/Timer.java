175703248a182ca6797fdf1cd200144073fc0e9f
xptraler <xptraler@inbox.lv>
2020-08-12 14:16:49 +0300

    commit 1

--- src/main/java/app/model/Timer.java
+++ src/main/java/app/model/Timer.java
@@ -1,6 +1,18 @@
 package app.model;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Timer {
+    private static Timer timer;
+
+    public static Timer getTimer() {
+        if (timer == null) {
+            timer = new Timer();
+        }
+        return timer;
+    }
 
     private Long nanoTime = System.nanoTime();
 
